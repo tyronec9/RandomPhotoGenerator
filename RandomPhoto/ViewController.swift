0b5dd11ba8adb6013581359c32016a45be979189
tyronec9 <109983232+tyronec9@users.noreply.github.com>
2023-04-03 16:29:22 -0400

    Optimized Ui to be more responsive

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -8,12 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal )
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemBlue,
+        .systemCyan,
+        .systemPink,
+        .systemMint,
+        .systemBrown,
+        .systemGray]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        view.addSubview(imageView)
+        imageView.frame = CGRect (x: 0, y:0 , width: 300 , height: 300)
+        imageView.center = view.center
+        
+        
+        
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-
-
+    @objc func didTapButton() {
+        getRandomPhoto()
+        
+        view.backgroundColor = colors.randomElement()
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(x:20,
+                              y: view.frame.size.height-50-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-40,
+                              height: 50)
+    }
+    
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        
+        DispatchQueue.global(qos: .background).async {
+            guard let data = try? Data(contentsOf: url) else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+    }
 }
 
